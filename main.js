762de2474af6da5804501ae3882241d8f55935ca
Jiyong Kim <luke.jiyongkim@gmail.com>
2023-04-27 22:17:35 -0400

    completed lab

--- main.js
+++ main.js
@@ -3,9 +3,33 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const emptyHeartIcon = document.getElementsByClassName('like-glyph')
 
+for (let icons of emptyHeartIcon) {
+ 
+icons.addEventListener('click', function() {
 
-
+mimicServerCall()
+  .then(function() {
+      if(icons.classList.contains('like-glyph')) {
+      icons.classList.replace('like-glyph','activated-heart',);
+      icons.innerText = FULL_HEART;
+      } else {
+      icons.classList.replace('activated-heart', 'like-glyph')
+      icons.innerText = EMPTY_HEART;
+  }
+})
+  .catch(function () {
+      const errorMessage = document.getElementById('modal')
+      errorMessage.classList.remove('hidden')
+    
+      setTimeout(function() {
+        errorMessage.classList.add('hidden')
+        } , 3000)
+      }
+    )
+  }
+  )}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
